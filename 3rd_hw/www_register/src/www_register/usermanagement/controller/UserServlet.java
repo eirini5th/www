ea787d29b7f16e9f7116f03fd6ddd7c16e749e0a
eirini5th <33671623+eirini5th@users.noreply.github.com>
2019-11-05 18:27:39 +0200

    registration code
    (Java, Servlet, MYSQL connection)

---
+++ 3rd_hw/www_register/src/www_register/usermanagement/controller/UserServlet.java
@@ -0,0 +1,55 @@
+package www_register.usermanagement.controller;
+
+//package net.javaguides.employeemanagement.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//import net.javaguides.employeemanagement.dao.EmployeeDao;
+//import net.javaguides.employeemanagement.model.Employee;
+import www_register.usermanagement.dao.UserDao;
+import www_register.usermanagement.model.User;
+
+
+@WebServlet("/register")
+public class UserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private UserDao userDao;
+
+    public void init() {
+    	userDao = new UserDao();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String address = request.getParameter("address");
+        String contact = request.getParameter("contact");
+
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setContact(contact);
+        user.setAddress(address);
+
+        try {
+            userDao.registerUser(user);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        response.sendRedirect("userdetails.jsp");
+    }
+}
